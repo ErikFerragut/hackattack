821929cb77cb495eb49fadf74dbc735fa2f8b510
jferragut666 <jacobferragut@gmail.com>
2015-07-07 19:16:25 -0400

    i moved the player_own to player obj

--- hackattack_gamestate.py
+++ hackattack_gamestate.py
@@ -1,8 +1,8 @@
 import random
 
 class GameState(object):
-    def __init__(self):
-        self.num_players = 5
+    def __init__(self, game):
+        self.game = game
         self.exploits_per_os = 5
         self.start_with_exploits = 4
         self.vuln_prob = 0.8
@@ -17,7 +17,7 @@ class GameState(object):
                 self.all_exploits.append( (o,i) )'''
         self.all_exploits = [ (o,i) for o in self.OSs for i in xrange(self.exploits_per_os) ]
         
-        self.num_hosts = 5*self.num_players
+        self.num_hosts = 5*self.game.num_players
 
 
         # (os, [vulnerabilities])
@@ -26,14 +26,10 @@ class GameState(object):
                               if random.random() < self.vuln_prob ]
                        for h in xrange(self.num_hosts) ]
 
-        a = range(self.num_hosts)
-        random.shuffle(a)
-        players_start = [ aa for aa in a[:self.num_players] ]
-        self.players_own = [ {s:1} for s in players_start ]
         self.players_expl = []
-        for i in xrange(self.num_players):
+        for i in xrange(self.game.num_players):
             random.shuffle(self.all_exploits)
             self.players_expl.append( self.all_exploits[:self.start_with_exploits] )
-        self.players_traced = [ set([]) for i in xrange(self.num_players) ]  # original
-        self.news = { p:[] for p in xrange(self.num_players) }
+        self.players_traced = [ set([]) for i in xrange(self.game.num_players) ]  # original
+        self.news = { p:[] for p in xrange(self.game.num_players) }
         self.game_round = 0
