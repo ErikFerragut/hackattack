e678b9da6c0b4f81a53d2f432877fcf0125d96fa
jferragut666 <jacobferragut@gmail.com>
2015-07-09 17:15:29 -0400

    fixed player error in working attacks

--- hackattack.py
+++ hackattack.py
@@ -53,7 +53,7 @@ class Game(object):
     def working_attacks(self,player, host):
         '''return a list of the short codes for attacks player has that work on machine'''
         # print "player {}'s exploits are {} (vuln = {})".format(player, self.state.players_expl[player], self.state.board_vuln[host])
-        return [ e for e in players.self.players_expl[player]
+        return [ e for e in self.players[player].players_expl
                  if self.state.board_os[host][0] == e[0] and int(e[1:]) in self.state.board_vuln[host] ]
                  
     def do_scan(self, move):
