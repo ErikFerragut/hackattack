debe64955cc3ca63b7cdff4680f7a74f519837ad
Wildgmax000 <wildgmax000@gmail.com>
2015-07-15 19:03:12 -0400

    added incomplete netplayer class

---
+++ hackattack_NetPlayer.py
@@ -0,0 +1,9 @@
+from hackattack_player import *
+
+class NetPlayer(Player):
+    def __init__(self, game, name, start):
+        
+        # do pre stuff
+        super(NetPlayer, self).__init__(game, name, start)
+        # do post stuff
+        
