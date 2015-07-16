ff7d51f00fec784f4c0e7048e768065d5b1adf37
supermariopie <supermariopie8@gmail.com>
2015-07-16 18:13:57 -0400

    AI's

--- hackattack_ai.py
+++ hackattack_ai.py
@@ -32,16 +32,16 @@ class AI(Player):
             self.say({'text':'You found a new exploit! ' + ne, 'type':'new exploit',
                       'exploit':ne})
     
-    # def update_output(self):
-        # pass
+    def update_output(self):
+        pass
     
 
-    # def turn_done(self):
-        # pass
+    def turn_done(self):
+        pass
         
     def say(self, said):
         '''How the player class receives messages from the game.'''
-        print said['text']
+        # print said['text']
 
         if 'type' not in said:
             said['type'] = 'not_given'
@@ -76,60 +76,59 @@ class AI(Player):
         
 class NathanAI(AI):
     def __init__(self, game, name, start):
-        super(NathanAI, self).__init__(game, name, start)
+        super(NathanAI, self).__init__()
         self.counter = 0
         
     def get_moves(self):
         moves = []
         
         for p in self.own:
-            # decide if patching is possible
-            print self.oss, self.players_expl, self.game.state.board_patches[p], "!!!"
+            
+            # decide whether to fortify or expand
             unpatched_exploits = [ e for e in self.players_expl 
-                 if e[0] == self.oss[p][0] and self.patches[p][int(e[1:])] != True ]
-            if len(unpatched_exploits) > 0: # fortify
-                moves.append({'player':self.game.state.player,'from':p,
-                              'action':'p', 'exploit':random.choice(unpatched_exploits)})
-            else:                     # expand
+                 if e[0] == self.'''oss'''[p] and int(e[1:]) not in self.patches[p] ]
+            if len(unpatched_exploits) !> 0: # fortify
                 moves.append({'player':self.game.state.player,
                               'action':'b', 'from':p})
-                
+            else:                     # expand
+                moves.append({'player':self.game.state.player,
+                              'action':'p', 'from':p,
+                              'to':random.randint(0,self.game.state.num_hosts),
+                              'exploit':random.choice(self.players_expl)})
         return moves
-
-'''
 class EthanAI(AI):
     import random
     def __init__(self, game, name, start):
         super(EthanAI, self).__init__()
-        easy_hacks = []
-        turns_since_c = ()
-        recon = moves.append({'player':self.game.state.player,'action':r, 'from':p,'to'random.randint(0,self.game.state.num_hosts)})#should not recon computers in known OSes
+        self.easy_hacks = []
+        self.turns_since_c = {n:0 for n in self.own} 
+        self.recon = moves.append({'player':self.game.state.player,'action':r, 'from':p,'to'random.randint(0,self.game.state.num_hosts)})
+        #should not recon computers in known OSes
     def update_lists():
-        easy_hacks.append(computer if you have an exploit for known OS and a player is on it)
-        turns_since_c
+        #self.easy_hacks.append(m for m in self.patches if any [l == False for l in self.patches[m]]
+            for i in self.turns_since_c:
+                self.turns_since_c[i] += 1
+        return
     def func1():
         when len(moves) < len(self.own)#:           
-            if #any in self.known accounts = any in self.own:
-                if len(easy_hacks) = 0:
-                    use other computer to backdoor by hacking then clean on original computer
-                else clean
-            elif len(easy_hacks) > 0:
-                moves.append({'player':self.game.state.player,
-                                  'action':'h', 'from':p,
-                                  'to':random.choice(easy_hacks),
-                                  'exploit':random expl with OS the same as target})
-            else moves.append({'player':self.game.state.player,
-                               'action':r, 'from':p,
-                               'to'rendom.randint(0,self.game.state.num_hosts)})#should not recon computers in known OSes
+            for i in xrange(self.own) if i in self.known_accounts:
+                {'player':self.game.state.player,'action':'c', 'from':i}
+                #if len(easy_hacks) = 0:
+                    #use other computer to backdoor by hacking then clean on original computer
+                # else clean
+            if len(easy_hacks) > 0:
+                moves.append({'player':self.game.state.player,'action':'h', 'from':p,'to':#j in random.choice(easy_hacks),'exploit':h in self.players_expl if [h] in [j]})
+            else moves.append({'player':self.game.state.player,'action':'r', 'from':p,'to'#i in self.game.state.num_hosts if i not in known_OSes)})
+            #should not recon computers in known OSes
         return moves
-    def war():
+    #def war():
         '''recon target if not already done
         hack target with three computers
         if some remain but you were cleaned hack with 1 - number removed
         if some remain and not cleaned clean then scan if nothing detected
         if all cleaned hack with 7 and follow same procedure, but if still cleaned label target Nathan'''
-    def func2():
-        when len(moves) < len(self.own) 
+    #def func2():
+        '''when len(moves) < len(self.own) 
             if ?.known accounts = self.own:
                     use other computer to backdoor by hacking then clean on original computer
             elif turns_since_c > random.randint(2,3)
@@ -138,7 +137,7 @@ class EthanAI(AI):
                 do them
             elif war possible:
                 start war
-            else return recon
+            else return recon'''
 
     update_lists    
     if len(self.own) < 3:
@@ -167,4 +166,3 @@ class Andrew(AI):
                               'action':'b', 'from':p})
         return moves
 
-'''
